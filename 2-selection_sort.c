d3195e28d11257033b4acd36d44a5504a544cf23
brerickner <breannarickner@gmail.com>
2021-01-30 22:12:14 +0000

    did not need curly braces upon realizing there's a single statement within for loop :P

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,10 +18,9 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 		for (j = i + 1; j < n; j++)
-		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-		}
+				
 			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 	}
