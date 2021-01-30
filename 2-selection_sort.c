6543b0f9479cf8f34d55ed51b258792e7edf4a25
brerickner <breannarickner@gmail.com>
2021-01-30 22:10:10 +0000

    inserted print_array function. Added curly braces to for loop. added a Null check for array.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,13 +11,19 @@ void selection_sort(int *array, size_t size)
 	int i, j, min_idx;
 	int n = size;
 
+	if (!array)
+		return;
+
 	for (i = 0; i < n - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < n; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-		swap(&array[min_idx], &array[i]);
+		}
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
 	}
 }
 
