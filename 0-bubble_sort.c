830a9a8afbea516274ea7eb79ca0d416cc10cd6a
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-29 21:14:05 -0600

    now prints each time

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,10 @@ void bubble_sort(int *array, size_t size)
 
         for (j = 0; j < n - i - 1; j++)
             if (array[j] > array[j + 1])
+            {
             swap(&array[j], & array[j + 1]);
+            print_array(array, n);
+            }
 }
 
 void swap(int *xp, int *yp)
