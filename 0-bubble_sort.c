153d1732d6ee241a781bec0665ec112f610c869c
brerickner <breannarickner@gmail.com>
2021-01-30 21:32:42 +0000

    made betty edits

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,22 +9,30 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int i, j;
-    int n = size;
+	int i, j;
+	int n = size;
 
-    for (i = 0; i < n - 1; i++)
-
-        for (j = 0; j < n - i - 1; j++)
-            if (array[j] > array[j + 1])
-            {
-            swap(&array[j], & array[j + 1]);
-            print_array(array, n);
-            }
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, n);
+			}
+		}
+	}
 }
 
+/**
+* swap - swaps ints
+* @xp: 1st int
+* @yp: 2nd int
+*/
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
