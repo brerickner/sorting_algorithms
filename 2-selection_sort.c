4696ed19a058f35bddac2f9b092a8fa7b8a5381b
brerickner <breannarickner@gmail.com>
2021-01-30 21:37:16 +0000

    betty edits to 2 selection sort. all good

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,21 +8,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-    int i, j, min_idx;
-    int n = size;
+	int i, j, min_idx;
+	int n = size;
 
-    for (i = 0; i < n - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        swap(&array[min_idx], &array[i]);
-    }
+	for (i = 0; i < n - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		swap(&array[min_idx], &array[i]);
+	}
 }
+
+/**
+ * swap - swaps ints
+ * @xp: 1st int
+ * @yp: 2nd int
+ */
+
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
